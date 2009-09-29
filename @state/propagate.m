18af04e4f01913b203e1979e910ff75c149be9e2
Ville Bergholm <ville.bergholm@iki.fi>
2009-09-29 01:16:42 +0000

    superoperator propagation, decoherence example

--- @state/propagate.m
+++ @state/propagate.m
@@ -1,43 +1,146 @@
-function out = propagate(s, H, t, func)
-% STATE/PROPAGATE  Propagate a quantum state in time using a Hamiltonian.
-%  out = propagate(s, H, t [, func])
+function out = propagate(s, H, t, varargin)
+% STATE/PROPAGATE  Propagate a quantum state in time using a Hamiltonian or a Liouvillian.
+%
+%  out = propagate(s, H, t [, out_func])
+%  out = propagate(s, H_func, t, steps [, out_func])
 %
 %  Propagates the state s using the Hamiltonian H for the time t,
 %  returns the resulting state.
 %  If t is a vector, returns a cell array containing the propagated
 %  state for each time point given in t.
 %
-%  If a function handle func is given, each propagated state is inputted
-%  to it and the result is given as output.
+%  out == expm(-i*H*t)*|s>
+%  out == inv_vec(expm(L*t)*vec(\rho_s))
+
 
 % Ville Bergholm 2008-2009
 % James Whitfield 2009
 
 
-if (nargin < 4)
-    func = @(x) x; % no function given, use a NOP
+if (nargin < 3)
+  error('Needs a state, Hamiltonian and time duration(s).');
+end
 
-    if (nargin < 3)
-      error('Needs a state, a Hamiltonian and a time duration.');
+out_func    = @(x,h)x; % if no out_func is given, use a NOP
+t_dependent = false; % time independent Hamiltonian by default
+
+if (isnumeric(H))
+  % time independent, no steps parameter
+  d = size(H, 2);
+  if (nargin == 4)
+    out_func = varargin{1};
+  end
+
+elseif (isa(H, 'function_handle'))
+  % time dependent
+  t_dependent = true;
+  d = size(H(0), 2);
+
+  if (nargin < 4)
+    error('Number of steps needed.');
+  else
+    steps = varargin{1};
+    if (nargin == 5)
+      out_func = varargin{2};
     end
+  end
+
+else
+  error('The Hamiltonian parameter has to be either a matrix or a function handle.')
 end
 
-if (size(s.data, 1) ~= size(H, 2))
+
+n = length(t); % number of time instances we are interested in
+temp = size(s.data, 1); % system dimension
+
+if (d == temp) % Hamiltonian evolution
+  if (~t_dependent)
+    if (n == 1)
+      % single output, don't bother with a list
+      out = out_func(u_propagate(s, expm(-i*H*t)), H);
+      return
+    end
+
+    % several time instances requested, use eigendecomposition
+    out = cell(n, 1);
+
+    [v, d] = eig(H); 
+    d = diag(d);
+    for k=1:n
+      U = v*diag(exp(-i*d*t(k)))*v';
+      out{k} = out_func(u_propagate(s, U), H);
+    end
+
+  else
+    T = t(end); % final time
+    dt = T/steps; % maximum step size
+    r  = 1; % index of next t we are interested in
+    tt = 0; % time now
+    h = H(0); % value of Hamiltonian now
+
+    while (tt < T)
+      step = min(dt, t(r)-tt);
+
+      % propagate, advance time
+      s = u_propagate(s, expm(-i*H*step));
+      tt = tt + step;
+      h = H(tt); % value of Hamiltonian now
+
+      % should we output something?
+      if (t(r) <= tt)
+	out{r} = out_func(s, h);
+	r = r + 1; % next output
+      end
+    end
+  end
+
+elseif (d == temp^2) % Liouvillian superoperator evolution
+
+  svec = vec(operator(s).data); % state operator, arranged as a col vector
+
+  if (~t_dependent)
+    if (n == 1)
+      % single output, don't bother with a list
+      [svec, err] = expv(t, H, svec);
+      s.data = inv_vec(svec);
+      out = out_func(s, H);
+      return
+    end
+
+    % several time instances requested
+    out = cell(n, 1);
+
+    [w, err] = expv(t, H, svec);
+    for k=1:n
+      s.data = inv_vec(w(:,k));
+      out{k} = out_func(s, H);
+    end
+
+  else
+    T = t(end); % final time
+    dt = T/steps; % maximum step size
+    r  = 1; % index into t
+    tt = 0; % time now
+    h = H(0); % value of Liouvillian now
+
+    while (tt < T)
+      step = min(dt, t(r)-tt);
+
+      % propagate, advance time
+      [svec, err] = expv(step, H, svec);
+
+      tt = tt + step;
+      h = H(tt); % value of Hamiltonian now
+
+      % should we output something?
+      if (t(r) <= tt)
+	s.data = inv_vec(svec);
+	out{r} = out_func(s, h);
+	r = r + 1; % next output
+      end
+    end
+  end
+
+else
   error('Dimension of the Hamiltonian does not match the dimension of the state.');
-end
-
-n = length(t);
-
-if (n == 1)
-  out = func(u_propagate(s, expm(-i*H*t)));
-  return
-end
-
-% several time instances requested, use eigendecomposition
-out = cell(n, 1);
-[v, d] = eig(H); 
-
-for k=1:n
-  U = v*diag(exp(-i*diag(d)*t(k)))*v';
-  out{k} = func(u_propagate(s, U));
 end
---
+++ examples/markov_decoherence.m
@@ -0,0 +1,59 @@
+function [H, D, b] = markov_decoherence(T1, T2, b)
+% MARKOV_DECOHERENCE  Decoherence demo.
+%
+%  [H, D, b] = markov_decoherence(T1, T2)
+%
+%  Given decoherence times T1 and T2, creates a markovian bath b
+%  and a coupling operator D which reproduce them on a single-qubit system.
+
+% Ville Bergholm 2009
+
+
+global qit;
+
+fprintf('\n\n=== Markovian decoherence ===\n')
+
+if (nargin < 2)
+  error('Both T1 and T2 must be given.')
+end
+
+omega0 = 1e10 % Hz
+T = 0.02 % K
+
+delta = 2.5 + rand; % qubit energy splitting
+
+% setup the bath
+if (nargin < 3)
+  b = bath('ohmic', omega0, T); % defaults
+end
+
+[b, H, D] = bath_fit(b, delta, T1, T2);
+L = lindblad.liouvillian(H, D, b);
+
+t = 0:0.1:10;
+
+% T1 demo
+s = state("1", [2]); % qubit in the |1> state
+out = propagate(s, L, t, @(x) ev(x, qit.p1));
+figure
+plot(t, cell2mat(out), 'r-', t, exp(-t/(T1*omega0)), 'b-');
+xlabel('t \omega_0');
+ylabel('probability');
+axis([0 t(end) 0 1])
+title('T_1: decay');
+legend('P_1', 'exp(-t/T_1)')
+
+
+% T2 demo
+s = state("0", [2]);
+s = u_propagate(s, R_y(pi/2)); % rotate to (|0>+|1>)/sqrt(2)
+out = propagate(s, L, t, @(x) ev(u_propagate(x, R_y(-pi/2)), qit.p0));
+figure
+plot(t, cell2mat(out), 'r-', t, 0.5*(1+exp(-t/(T2*omega0))), 'b-');
+xlabel('t \omega_0');
+ylabel('probability')
+axis([0 t(end) 0 1])
+title('T_2: dephasing');
+legend('P_0', '0.5*(1+exp(-t/T_2))')
+
+end
