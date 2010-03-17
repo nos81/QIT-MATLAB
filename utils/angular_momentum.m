ff5685bbe512b4d3074b24ee641419ddcd2243d2
Ville Bergholm <ville.bergholm@iki.fi>
2010-03-17 22:23:51 +0000

    caching for angular_momentum ops

--- utils/angular_momentum.m
+++ utils/angular_momentum.m
@@ -1,14 +1,27 @@
-function [Jx, Jy, Jz] = angular_momentum(j)
+function J = angular_momentum(j)
 % ANGULAR_MOMENTUM  Angular momentum matrices.
-%  [Jx, Jy, Jz] = angular_momentum(j)
+%  J = {Jx, Jy, Jz} = angular_momentum(j)
 %
 %  Returns the angular momentum matrices \vec(J)/\hbar
-%  for the 2*j + 1 -dimensional subspace defined by quantum number j.
+%  for the 2*j + 1 -dimensional subspace defined by the
+%  quantum number j as a cell vector.
 
 % Ville Bergholm 2009-2010
 
 
+global qit;
+
 n = 2*j + 1; % dimension
+
+if (n < 1)
+  error('Dimension must be one or greater.')
+end
+
+% check cache first
+if (length(qit.angular_momentum) >= n && length(qit.angular_momentum{n}) > 0)
+  J = qit.angular_momentum{n};
+  return;
+end
 
 % raising operator in subspace J^2 = j*(j+1)
 m = j;
@@ -24,6 +37,11 @@ Jminus = Jplus';
 % Jplus  = Jx + i*Jy
 % Jminus = Jx - i*Jy
 
-Jx = 0.5*(Jplus + Jminus);
-Jy = 0.5*i*(Jminus - Jplus);
-Jz = spdiags([j:-1:-j].', 0, n, n);
+J = cell(1,3);
+J{1} = 0.5*(Jplus + Jminus);
+J{2} = 0.5*i*(Jminus - Jplus);
+J{3} = spdiags([j:-1:-j].', 0, n, n);
+
+
+% store them in the cache
+qit.angular_momentum{n} = J;
--- utils/fidelity.m
+++
@@ -1,9 +0,0 @@
-function f = fidelity(A, B)
-% FIDELITY  Relative fidelity of two state operators/matrices.
-%  f = fidelity(A, B)
-%
-%  Returns trace(A*B).
-
-% Ville Bergholm 2007-2009
-
-f = trace(A*B);
