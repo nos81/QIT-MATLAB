608c67ce9751b48985f228674687bf90901b0aa3
Ville Bergholm <ville.bergholm@iki.fi>
2010-04-22 20:54:08 +0000

    unnecessary

--- +plots/bloch_sphere.m
+++ +plots/bloch_sphere.m
@@ -8,8 +8,8 @@ function h = bloch_sphere(s)
 %
 %  s is a two dimensional state to be plotted.
 
+% Ville Bergholm  2005-2010
 % James Whitfield 2010
-% Ville Bergholm  2005-2009
 
 
 [X,Y,Z] = sphere(40);
@@ -26,8 +26,9 @@ zlabel('z');
 plot3(0,0,1,'r.');
 plot3(0,0,-1,'b.');
 
-text(0, 0,  1.1, '|0>');
-text(0, 0, -1.1, '|1>');
+text(0, 0,  1.1, '|0\rangle');
+text(0, 0, -1.1, '|1\rangle');
 if nargin==1
-    plots.vectarrow([0;0;0],bloch_vector(s));
+    v = bloch_vector(s);
+    quiver3(0, 0, 0, v(1), v(2), v(3), 0);
 end
