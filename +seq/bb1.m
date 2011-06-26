a8d1c27c68798a177f8483e3cbf74a9a96b6e38a
Ville Bergholm <ville.bergholm@iki.fi>
2011-06-26 17:55:02 +0000

    BB1 sequence fixed.

--- +seq/bb1.m
+++ +seq/bb1.m
@@ -18,7 +18,7 @@ if (nargin < 3)
 end
 
 ph1 = acos(-theta/(4*pi));
-W1  = seq.nmr([pi, ph1; -2*pi, 3*ph1; -pi, ph1]);
+W1  = seq.nmr([pi, ph1; 2*pi, 3*ph1; pi, ph1]);
 
 %s = [W1; seq.nmr([theta, phi])];
 s = [seq.nmr([location*theta, phi]); W1; seq.nmr([(1-location)*theta, phi])];
