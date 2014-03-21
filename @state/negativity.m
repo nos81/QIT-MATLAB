06466226da9beebd7646ff733f713d14c5cbccfd
Ville Bergholm <ville.bergholm@iki.fi>
2014-03-21 16:38:31 +0000

    state/negativity bugfix.

--- @state/negativity.m
+++ @state/negativity.m
@@ -7,7 +7,7 @@ function N = negativity(s, sys)
 
 %! A. Peres, "Separability Criterion for Density Matrices", PRL 77, 1413 (1996).
 %! M. Horodecki et al., "Separability of Mixed States: Necessary and Sufficient Conditions", Physics Letters A 223, 1-8 (1996).
-% Ville Bergholm 2008
+% Ville Bergholm 2008-2014
 
 
 if (nargin < 2)
@@ -17,4 +17,4 @@ end
 s = ptranspose(s, sys); % partial transpose the state
 x = svd(s.data); % singular values
 
-N = (sum(sqrt(x))-1)/2;
+N = (sum(x) -1) / 2;
