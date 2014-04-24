f7043e686e8acbccd5cd33d2ca4bf63f65bf480c
Ville Bergholm <ville.bergholm@iki.fi>
2014-04-24 19:35:49 +0200

    hamiltonian/heisenberg updated.

--- +hamiltonian/heisenberg.m
+++ +hamiltonian/heisenberg.m
@@ -1,107 +1,122 @@
-function varargout = heisenberg(dim, J, h, ss)
-% HEISENBERG  Heisenberg model, 1D spin chain.
-%  [H, dim] = heisenberg(dim, J, h)               % full Hamiltonian
-%  [H, dim] = heisenberg(dim, J, h, [first last]) % partial H
-%  [S_k, S_{k+1}, h_k, h_{k+1}] = heisenberg(dim, J, h, k) % operators
+function [H, dim] = heisenberg(dim, C, J, B)
+% HEISENBERG  Heisenberg spin network model
+%  [H, dim] = heisenberg(dim, C, J, B)
 %
-%  Returns the Hamiltonian H for an implementation of the
-%  Heisenberg model, describing a one-dimensional chain of spins
-%  with nearest-neighbor couplings in an external magnetic
-%  field in the z direction.
+%  Returns the Hamiltonian H for the Heisenberg model, describing a
+%  network of n interacting spins in an external magnetic field.
 %
-%  The secondary calling syntax returns the coupling (S1, S2) and
-%  local (h1, h2) operators just for sites k and k+1 in the chain.
+%  dim is a dimension vector of the spins, i.e. dim == [2 2 2]
+%  would be a system of three spin-1/2's.
 %
-%  dim is a vector of the dimensions of the spins, i.e. dim == [2 2 2]
-%  would be a chain of three spin-1/2's.
+%  C is the n \times n connection matrix of the spin network, where C(i, j)
+%  is the coupling strength between spins i and j. Only the upper triangle is used.
 %
-%  J defines the coupling strengths in the chain. It's either a
-%  3x1 vector (homogeneous couplings) or a function handle
-%  J(site, component) (site-dependent couplings).
+%  J defines the form of the spin-spin interaction. It is either a
+%  3-vector (homogeneous couplings) or a function J(i, j) returning
+%  a 3-vector for site-dependent interactions.
+%  Element k of the vector is the coefficient of the Hamiltonian term S_k^{(i)} S_k^{(j)},
+%  where S_k^{(i)} is the k-component of the angular momentum of spin i.
 %
-%  By setting J = [0 0 a] the Ising model is obtained.
-%  Likewise,  J = [a a 0] gives the XY model.
+%  B defines the effective magnetic field the spins locally couple to. It's either
+%  a 3-vector (homogeneous field) or a function B(i) that returns a 3-vector for
+%  site-dependent field.
 %
-%  h defines the couplings of the spins to the magnetic field.
-%  It's either a scalar (homogeneous) or a function handle h(site)(site-dependent).
+%  Examples:
+%    C = diag(ones(1, n-1), 1)  linear n-spin chain
+%    J = [2 2 2]                isotropic Heisenberg coupling
+%    J = [2 2 0]                XX+YY coupling
+%    J = [0 0 2]                Ising ZZ coupling
+%    B = [0 0 1]                homogeneous Z-aligned field
 %
-%  H = \sum_k (\sum_{a = x,y,z} J(k,a) Sa_{k} Sa_{k+1}) +h(k) Sz_{k})
+%  H =  \sum_{\langle i,j \rangle} \sum_{k = x,y,z} J(i,j)[k] S_k^{(i)} S_k^{(j)}
+%      +\sum_i \vec{B}(i) \cdot \vec{S}^{(i)})
 
-% Ville Bergholm 2009-2010
+% Ville Bergholm 2009-2014
 
 
-if (nargin < 3 || nargin > 4)
-  error('Usage: heisenberg(dim, J, h)')
+if nargin < 1
+    error('Usage: heisenberg(dim, C, J, B)')
+end
+n = length(dim);
+
+if nargin < 2
+    % linear open chain
+    C = 'open';
+end
+if nargin < 3
+    % Ising ZZ
+    J = [0, 0, 2];
+end
+if nargin < 4
+    % Z field
+    B = [0, 0, 1];
 end
 
-if (isa(J, 'function_handle'))
-  Jfunc = J;
-else
-  if (isvector(J) && length(J) == 3)
-    Jfunc = @(k,s) J(s);
-  else
-    error(['J must be either a 3x1 vector or a function handle.'])
-  end
-end
-
-if (isa(h, 'function_handle'))
-  hfunc = h;
-else
-  if (isscalar(h))
-    hfunc = @(k) h;
-  else
-    error('h must be either a scalar or a function handle.')
-  end
-end
-
-if (~isvector(dim))
-  error('Only 1D lattices supported for now.')
-end
-n = size(dim);
-n = prod(n);
-
-
-if (nargin == 4)
-  if (isscalar(ss))
-    % just return the ops for sites ss and ss+1
-    S1 = angular_momentum(dim(ss));
-    S2 = angular_momentum(dim(ss+1));
-
-    for k=1:3
-      varargout{1}{k} = Jfunc(ss, k) * S1{k}; % include J in S1
+% simple way of defining some common cases
+if ischar(C)
+    switch C
+      case 'open'
+        C = diag(ones(1, n-1), 1);
+      case 'closed'
+        C = diag(ones(1, n-1), 1);
+        C(1, end) = 1;
+      otherwise
+        error('Unknown topology.')
     end
-    varargout{2} = S2;
-    varargout{3} = hfunc(ss)   * S1{3}; % h1
-    varargout{4} = hfunc(ss+1) * S2{3}; % h2
-    return
-  else
-    % return partial H, ss == [start, end] (range of sites)
-  end
+end
+
+if isa(J, 'function_handle')
+    Jfunc = J;
 else
-  ss = [1, length(dim)]; % H for entire chain (default)
+    if isvector(J) && length(J) == 3
+        Jfunc = @(i, j) C(i, j) * J;
+    else
+        error('J must be either a 3x1 vector or a function handle.')
+    end
 end
 
-
-H = sparse(0);
-S1 = angular_momentum(dim(ss(1))); % spin ops for first site
-
-rdim = dim(ss(1):ss(2)); % reduced dimension vector
-m = 1; % reduced subsystem index to go with rdim
-
-for k=ss(1):ss(2)-1
-  % coupling between spins k and k+1: S1 \cdot S2
-  S2 = angular_momentum(dim(k+1)); % spin ops
-
-  H = H +op_list({{Jfunc(k,1)*S1{1}, m; S2{1}, m+1}, {Jfunc(k,2)*S1{2}, m; S2{2}, m+1},...
-                  {Jfunc(k,3)*S1{3}, m; S2{3}, m+1}, {hfunc(k)*S1{3}, m}}, rdim);
-
-  % ops for next iteration
-  S1 = S2;
-  m = m+1;
+if isa(B, 'function_handle')
+    Bfunc = B;
+else
+    if isvector(B) && length(B) == 3
+        Bfunc = @(i) B;
+    else
+        error('B must be either a 3x1 vector or a function handle.')
+    end
 end
 
-% final term
-H = H +op_list({{hfunc(ss(2)) * S1{3}, m}}, rdim);
+% local magnetic field terms
+temp = {};
+for i = 1:n
+    A = angular_momentum(dim(i));  % spin ops
+    temp = cat(2, temp, {{cdot(Bfunc(i), A), i}});
+end
+H = op_list(temp, dim);
 
-varargout{1} = H;
-varargout{2} = rdim;
+% spin-spin couplings: loop over nonzero entries of C
+% only use the upper triangle
+[a, b] = find(triu(C));
+for m = 1:length(a)
+    i = a(m);
+    j = b(m);
+    % spin ops for sites i and j
+    Si = angular_momentum(dim(i));
+    Sj = angular_momentum(dim(j));
+    temp = {};
+    % coupling between sites a and b
+    c = Jfunc(i, j);
+    for k = 1:3
+        temp = cat(2, temp, {{c(k) * Si{k}, i; Sj{k}, j}});
+        H = H +op_list(temp, dim);
+    end
+end
+end
+
+function res = cdot(v, A)
+% Real dot product of a vector with a cell vector of operators.
+
+res = 0;
+for k = 1:length(v)
+    res = res +v(k) * A{k};
+end
+end
