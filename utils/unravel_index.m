966f2be82a93040eca3d0d2a0ea1597b9300b9a1
Ville Bergholm <ville.bergholm@iki.fi>
2015-05-03 00:22:32 +0300

    unravel_index works on vectors of indices.

--- utils/unravel_index.m
+++ utils/unravel_index.m
@@ -7,13 +7,13 @@ function ket = unravel_index(ind, dim)
 %
 %  Uses the MATLAB indexing convention (first index is 1, not zero).
 
-% Ville Bergholm 2008-2014
+% Ville Bergholm 2008-2015
 
 
 d = fliplr(dim); % the big-endian convention makes this way more complicated than it should be
 n = length(dim);
 for k = 1:n % start from least significant digit
     %ind2sub with 2 output parms uses up the first dim given
-    [ket(k), ind] = ind2sub(d(k:n), ind);
+    [ket(:,k), ind] = ind2sub(d(k:n), ind);
 end
 ket = fliplr(ket); % big-endian again
