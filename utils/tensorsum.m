99d8e131ff97cf3aac172f064b162123924a4ee6
Ville Bergholm <ville.bergholm@iki.fi>
2016-04-16 19:59:15 +0300

    utils/tensorsum updated.

--- utils/tensorsum.m
+++ utils/tensorsum.m
@@ -1,11 +1,21 @@
-function c = tensorsum(a, b)
+function res = tensorsum(varargin)
 % TENSORSUM  Like kron but adding instead of multiplying.
     
-% Ville Bergholm 2008-2010
+% Ville Bergholm 2008-2016
 
-    
+
 %c = log(kron(exp(a), exp(b))); % a perverted way of doing it, the exp overflows...
-c = [];
-for k=1:length(a)
-  c = [c, a(k)+b];
+
+res = 0;
+for k = 1:nargin
+    temp = [];
+    for a=1:size(res, 1)
+        row = [];
+        for b=1:size(res, 2)
+            foo = res(a,b) +varargin{k};
+            row = [row, foo];
+        end
+        temp = [temp; row];
+    end
+    res = temp;
 end
