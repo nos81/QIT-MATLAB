18b75fdec9174797059d500b37f4e0fe59cfa241
Ville Bergholm <ville.bergholm@iki.fi>
2016-10-10 15:34:43 +0200

    utils/rand_GL

---
+++ utils/rand_GL.m
@@ -0,0 +1,13 @@
+function S = rand_GL(n)
+% RAND_GL  Random GL(n,C) matrix.
+%  S = rand_GL(n)
+%
+%  Returns a random general linear n*n matrix.
+%  NOTE: The randomness is not defined in any deeply meaningful sense.
+%  The det(S) ~= 0 condition is for now fulfilled only statistically,
+%  i.e. you almost never obtain a non-invertible matrix.
+
+% Ville Bergholm 2016
+
+
+S = randn(n) +1i*randn(n);
